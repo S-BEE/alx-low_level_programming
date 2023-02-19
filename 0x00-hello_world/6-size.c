53092918b99d2b85657910631d491c5c370ecde4
S-BEE <bshukurah96@gmail.com>
2023-02-19 01:48:34 -0800

    size32 vs 64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,14 @@
 /**
  *main - Entry point
  *Return: Always 0 (Success)
+ *You might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
 */
 int main(void)
 {
-	printf("Size of a char: %d byte(s) ", sizeof(char));
-	printf("Size of an int: %d byte(s) ", sizeof(int));
-	printf("Size of a long int:%d byte(s) ", sizeof(long int));
-	printf("Size of a long long int: %d byte(s) ", sizeof(long long int));
-	printf("size of a float: %d byte(s) ", sizeof(float));
+	printf("Size of a char: %lu byte(s) \n", sizeof(char));
+	printf("Size of an int: %lu byte(s) \n", sizeof(int));
+	printf("Size of a long int:%lu byte(s) \n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s) \n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n ", sizeof(float));	
+	return (0);
 }
