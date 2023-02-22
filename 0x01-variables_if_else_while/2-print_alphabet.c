071b1ec093f03837570a3b8bbf8d44fb55de6fda
S-BEE <bshukurah96@gmail.com>
2023-02-21 18:05:53 -0800

    prints a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *description: prints the alphabets in lower case
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++);
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
